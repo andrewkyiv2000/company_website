e1677d9cffa7c4c523d5df1f9e4e1dcd13c567cb
andrewkyiv2000 <videodesign2014@gmail.com>
2022-03-31 14:16:02 +0300

    upper navigation menu done basically

---
+++ src/components/navbar/menucontent.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const MenuContent = () => {
+  return (
+    <div>
+      <div className="gpt3__navbar-menu_container scale-up-center">
+        <div className="gpt3__navbar-menu_container-links">
+          <div className="hambmenu">
+            <p>
+              <a href="#home">Home</a>
+            </p>
+            <p>
+              <a href="#about">About</a>
+            </p>
+            <p>
+              <a href="#possibility">What we offer?</a>
+            </p>
+            <p>
+              <a href="#features">Portfolio</a>
+            </p>
+
+            <p>
+              <a href="#blog">Blog</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MenuContent;
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,15 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
-import { RiCloseLine, RiMenu3Line } from "react-icons/ri";
+//import { FaPencilAlt } from "react-icons/fa";
+import logo from "../../assets/logo.svg";
+import crossIcon from "../../assets/close_icon.png";
+import hambIcon from "../../assets/hamburger_icon.png";
+import MenuContent from "./menucontent.js";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo"></img>
         </div>
-        Navbar
+        <div className="gpt3__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#about">About</a>
+          </p>
+          <p>
+            <a href="#possibility">What we offer?</a>
+          </p>
+          <p>
+            <a href="#features">Portfolio</a>
+          </p>
+          <p>
+            <a href="#blog">Blog</a>
+          </p>
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <div>
+          <p>Sign In</p>
+        </div>
+        <div>
+          <button type="button">Sign Up</button>
+        </div>
+      </div>
+      <div className="gpt3__navbar-menu">
+        <a>
+          <span>
+            {toggleMenu ? (
+              <img src={crossIcon} onClick={() => setToggleMenu(false)} />
+            ) : (
+              <img src={hambIcon} onClick={() => setToggleMenu(true)} />
+            )}
+            {toggleMenu && (
+              <div>
+                <MenuContent />
+                <div className="gpt3__navbar-menu_container-links-sign">
+                  <div>
+                    <p>Sign In</p>
+                  </div>
+                  <div>
+                    <button type="button">Sign Up</button>
+                  </div>
+                </div>
+              </div>
+            )}
+          </span>
+        </a>
       </div>
     </div>
   );
